3f68ebeac7850c589434103550f24fde473fc26a
TahirReyaz <tahir200213@st.jmi.ac.in>
2021-12-16 20:15:44 +0530

    Initial commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ExpenseItem = () => {
+  return (
+    <div>
+      <div>March 28th 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$3534.34</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
